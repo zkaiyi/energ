76c0215abe4716028ac93fde465e8cfbd380a6d1
zkaiyi <1149189347@qq.com>
2019-01-04 11:43:38 +0800

    区域

--- js/info-01.js
+++ js/info-01.js
@@ -170,147 +170,147 @@ $(function(){
 });
 
 // 饼图
-$(function () {
-    var dom = document.getElementById("bt");
-    var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
-    app.title = '环形图';
-
-    option = {
-        tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b}: {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            x: 'right',
-            data:['1','2','3','4','5']
-        },
-        series: [
-            {
-                name:'访问来源',
-                type:'pie',
-                radius: ['50%', '70%'],
-                avoidLabelOverlap: false,
-                label: {
-                    normal: {
-                        show: false,
-                        position: 'center'
-                    },
-                    emphasis: {
-                        show: true,
-                        textStyle: {
-                            fontSize: '30',
-                            fontWeight: 'bold'
-                        }
-                    }
-                },
-                labelLine: {
-                    normal: {
-                        show: false
-                    }
-                },
-                data:[
-                    {value:335, name:'1'},
-                    {value:310, name:'2'},
-                    {value:234, name:'3'},
-                    {value:135, name:'4'},
-                    {value:1548, name:'5'}
-                ]
-            }
-        ]
-    };
-    ;
-    if (option && typeof option === "object") {
-        myChart.setOption(option, true);
-    }
-});
+// $(function () {
+//     var dom = document.getElementById("bt");
+//     var myChart = echarts.init(dom);
+//     var app = {};
+//     option = null;
+//     app.title = '环形图';
+//
+//     option = {
+//         tooltip: {
+//             trigger: 'item',
+//             formatter: "{a} <br/>{b}: {c} ({d}%)"
+//         },
+//         legend: {
+//             orient: 'vertical',
+//             x: 'right',
+//             data:['1','2','3','4','5']
+//         },
+//         series: [
+//             {
+//                 name:'访问来源',
+//                 type:'pie',
+//                 radius: ['50%', '70%'],
+//                 avoidLabelOverlap: false,
+//                 label: {
+//                     normal: {
+//                         show: false,
+//                         position: 'center'
+//                     },
+//                     emphasis: {
+//                         show: true,
+//                         textStyle: {
+//                             fontSize: '30',
+//                             fontWeight: 'bold'
+//                         }
+//                     }
+//                 },
+//                 labelLine: {
+//                     normal: {
+//                         show: false
+//                     }
+//                 },
+//                 data:[
+//                     {value:335, name:'1'},
+//                     {value:310, name:'2'},
+//                     {value:234, name:'3'},
+//                     {value:135, name:'4'},
+//                     {value:1548, name:'5'}
+//                 ]
+//             }
+//         ]
+//     };
+//     ;
+//     if (option && typeof option === "object") {
+//         myChart.setOption(option, true);
+//     }
+// });
 
 // 折现图
-$(function () {
-    var dom = document.getElementById("zxt");
-    var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
-    app.title = '折柱混合';
-
-    option = {
-        tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-                type: 'cross',
-                crossStyle: {
-                    color: '#999'
-                }
-            }
-        },
-        toolbox: {
-            feature: {
-                // dataView: {show: true, readOnly: false},
-                magicType: {show: true, type: ['line', 'bar']},
-                // restore: {show: true},
-                // saveAsImage: {show: true}
-            }
-        },
-        // legend: {
-        //     data:['客户共区域用电','生产区域用电','平均用水量']
-        // },
-        xAxis: [
-            {
-                type: 'category',
-                data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
-                axisPointer: {
-                    type: 'shadow'
-                }
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value',
-                name: '电量/kwh',
-                min: 0,
-                max: 250,
-                interval: 50,
-                axisLabel: {
-                    formatter: '{value}'
-                }
-            },
-            {
-                type: 'value',
-                name: '平均电量/kwh',
-                min: 0,
-                max: 250,
-                interval: 50,
-                axisLabel: {
-                    formatter: '{value}'
-                }
-            }
-        ],
-        series: [
-            {
-                name:'客户共区域用电',
-                type:'bar',
-                data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-            {
-                name:'生产区域用电',
-                type:'bar',
-                data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-            },
-            {
-                name:'平均用水量',
-                type:'line',
-                yAxisIndex: 1,
-                data:[2.0, 2.4, 5.3, 30.6, 25.0,86.3,154.6,183.6,60.1,32.0,14.0,7.5]
-            }
-        ]
-    };
-    ;
-    if (option && typeof option === "object") {
-        myChart.setOption(option, true);
-    }
-});
+// $(function () {
+//     var dom = document.getElementById("zxt");
+//     var myChart = echarts.init(dom);
+//     var app = {};
+//     option = null;
+//     app.title = '折柱混合';
+//
+//     option = {
+//         tooltip: {
+//             trigger: 'axis',
+//             axisPointer: {
+//                 type: 'cross',
+//                 crossStyle: {
+//                     color: '#999'
+//                 }
+//             }
+//         },
+//         toolbox: {
+//             feature: {
+//                 // dataView: {show: true, readOnly: false},
+//                 magicType: {show: true, type: ['line', 'bar']},
+//                 // restore: {show: true},
+//                 // saveAsImage: {show: true}
+//             }
+//         },
+//         // legend: {
+//         //     data:['客户共区域用电','生产区域用电','平均用水量']
+//         // },
+//         xAxis: [
+//             {
+//                 type: 'category',
+//                 data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+//                 axisPointer: {
+//                     type: 'shadow'
+//                 }
+//             }
+//         ],
+//         yAxis: [
+//             {
+//                 type: 'value',
+//                 name: '电量/kwh',
+//                 min: 0,
+//                 max: 250,
+//                 interval: 50,
+//                 axisLabel: {
+//                     formatter: '{value}'
+//                 }
+//             },
+//             {
+//                 type: 'value',
+//                 name: '平均电量/kwh',
+//                 min: 0,
+//                 max: 250,
+//                 interval: 50,
+//                 axisLabel: {
+//                     formatter: '{value}'
+//                 }
+//             }
+//         ],
+//         series: [
+//             {
+//                 name:'客户共区域用电',
+//                 type:'bar',
+//                 data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+//             },
+//             {
+//                 name:'生产区域用电',
+//                 type:'bar',
+//                 data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+//             },
+//             {
+//                 name:'平均用水量',
+//                 type:'line',
+//                 yAxisIndex: 1,
+//                 data:[2.0, 2.4, 5.3, 30.6, 25.0,86.3,154.6,183.6,60.1,32.0,14.0,7.5]
+//             }
+//         ]
+//     };
+//     ;
+//     if (option && typeof option === "object") {
+//         myChart.setOption(option, true);
+//     }
+// });
 
 // 日常
 $(function () {
--- js2/info.js
+++ js2/info.js
@@ -1,3 +1,52 @@
+
+// 饼图
+var myChart = echarts.init(document.getElementById("bt"));
+option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        x: 'right',
+        data:['公共','生产','照明']
+    },
+    series: [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[
+                // {name:'公共'},
+                // {name:'生产'},
+                // {name:'照明'}
+            ]
+        }
+    ]
+};
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
+
 // 获取各类型总能
 function getzongenergy(ID,Type,STime,ETime) {
     var json = {
@@ -28,7 +77,22 @@ function getzongenergy(ID,Type,STime,ETime) {
             $(".ZHgg").text(res.pub);
             $(".ZHsc").text(res.pro);
             $(".ZHzm").text(res.lig);
+            $(".info-02-mode-fl-02-span").text(res.pub + res.pro + res.lig );
+            
 
+
+            myChart.setOption({
+                series: [
+                    {
+                        data:[
+                            {value:res.pub,name:'公共'},
+                            {value:res.pro,name:'生产'},
+                            {value:res.lig,name:'照明'}
+                        ]
+                    }
+                ]
+
+            });
 
         },error:function (xml) {
             console.log(xml)
@@ -39,12 +103,152 @@ function getzongenergy(ID,Type,STime,ETime) {
 
 
 }
+
+
+// 折线图
+    var myChart02 = echarts.init(document.getElementById("zxt"));
+    option = null;
+    option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'cross',
+                crossStyle: {
+                    color: '#999'
+                }
+            }
+        },
+        toolbox: {
+            feature: {
+                magicType: {show: true, type: ['line', 'bar']},
+            }
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: [],
+                axisPointer: {
+                    type: 'shadow'
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                name: '用量',
+                min: 0,
+                // max: 250,
+                // interval: 50,
+                axisLabel: {
+                    formatter: '{value}'
+                }
+            },
+            {
+                type: 'value',
+                name: '平均用量',
+                min: 0,
+                // max: 250,
+                // interval: 50,
+                axisLabel: {
+                    formatter: '{value}'
+                }
+            }
+        ],
+        series: []
+    };
+    ;
+    if (option && typeof option === "object") {
+        myChart02.setOption(option, true);
+    }
+
+function getzongxian(ID,Type,STime,ETime) {
+    var json02 = {
+        ID:ID,
+        Type:Type,
+        STime:STime,
+        ETime:ETime
+    };
+    $.ajax({
+        type:'post',
+        url:callurl + '/Region/Data/GetTypeDataList',
+        dataType: 'json',
+        data:json02,
+        success:function (res02) {
+            console.log(res02);
+
+            $(".mode03-fl-title-data").text(res02.Time[0])
+
+            myChart02.setOption({
+                xAxis: [
+                    {
+                        data: res02.Time
+                    }
+                ],
+                series: [
+                    {
+                        name:'公共区域用量',
+                        type:'bar',
+                        data:res02.PubData
+                    },
+                    {
+                        name:'生产区域用量',
+                        type:'bar',
+                        data:res02.Produce
+                    },
+                    {
+                        name:'照明区域用量',
+                        type:'bar',
+                        data:res02.Lighting
+                    },
+                    {
+                        name:'区域平均用量',
+                        type:'line',
+                        yAxisIndex: 1,
+                        data:res02.Average
+                    }
+                ]
+
+            });
+
+        },error:function (xml02) {
+            console.log(xml02)
+        }
+    });
+}
+
+
 // 默认状态下
 // 初始总量
 var Type = "1";
 var STime = "";
 var ETime = "";
 getzongenergy("0",Type,STime,ETime);
+getzongxian("0",Type,STime,ETime);
 sessionStorage.setItem("Type01",Type);
 sessionStorage.setItem("STime01",STime);
 sessionStorage.setItem("ETime01",ETime);
+
+$(document).on("click",".info-choose01-t-list",function () {
+    $(this).index();
+    var ZLindex = $(this).index();
+    if(ZLindex == "0"){
+        var Type="2";
+        sessionStorage.setItem("Type",Type);
+    }else if(ZLindex =="1"){
+        var Type="1";
+        sessionStorage.setItem("Type",Type);
+    }else if(ZLindex == "2"){
+        var Type="3";
+        sessionStorage.setItem("Type",Type);
+    }
+    var STime = sessionStorage.getItem("STime");
+    var ETime = sessionStorage.getItem("ETime");
+    getzongenergy("0",Type,STime,ETime);
+    getzongxian("0",Type,STime,ETime);
+});
+
+
+
+
+
+
